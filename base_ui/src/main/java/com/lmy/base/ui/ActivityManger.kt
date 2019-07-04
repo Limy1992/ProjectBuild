158112a6b1d8fc7fd73ab978d053bf6116c782ab
limy <limy_1992@163.com>
2019-07-04 13:55:50 +0800

    管理activity

--- base_ui/src/main/java/com/lmy/base/ui/ActivityManger.kt
+++ base_ui/src/main/java/com/lmy/base/ui/ActivityManger.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import androidx.fragment.app.FragmentActivity
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * activity管理
@@ -11,21 +12,59 @@ import java.util.*
  * Author lmy
  */
 object ActivityManger {
+    //打开的activity管理
     private val stack = Stack<Activity>()
+    //被标记批量关闭activity
+    private var finishActivity: Stack<Activity>? = null
 
-    fun addActivity(activity: Activity){
+
+    fun addActivity(activity: Activity) {
         stack.add(activity)
     }
 
-    fun removeActivity(activity: Activity){
+    fun removeActivity(activity: Activity) {
         stack.remove(activity)
+        finishActivity?.remove(activity)
     }
 
-    fun getFragmentActivity():FragmentActivity{
+    fun getFragmentActivity(): FragmentActivity {
         return stack.lastElement() as FragmentActivity
     }
 
-    fun getContext():Context{
+    fun getContext(): Context {
         return stack.lastElement()
+    }
+
+    /**
+     * 当前栈顶是否已经添加当前activity
+     */
+    fun istTopFlagBatchActivity(activity: Activity): Boolean {
+        return finishActivity?.lastElement() == activity
+    }
+
+    /**
+     * 标记批量关闭activity
+     */
+    fun flagBatchActivity(activity: Activity) {
+        if (finishActivity == null) {
+            finishActivity = Stack()
+        }
+
+        finishActivity?.add(activity)
+    }
+
+    /**
+     * 批量关闭已经标记的activity
+     */
+    fun finishBatchActivity() {
+        val finishAct = ArrayList<Activity>()
+        finishAct.addAll(finishActivity ?: ArrayList())
+        finishActivity?.clear()
+        finishActivity = null
+        for (activity in finishAct) {
+            if (!activity.isFinishing && !activity.isDestroyed) {
+                activity.finish()
+            }
+        }
     }
 }
